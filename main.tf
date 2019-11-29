3833377cf6f124f88774db263b39498acc93030e
SudhinNandan <57177849+SudhinNandan@users.noreply.github.com>
2019-11-29 10:44:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "azurerm" {
   version         = "=1.27"
-  subscription_id = "${var.subscription_id}"
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "main" {
   name     = "sitecorepocfull"
-  location = "${var.location}"
+  location =  var.location
 }
 
 # This creates the plan that the service use
 resource "azurerm_app_service_plan" "main" {
-  name                = "${var.prefix}-asp"
+  name                = var.prefix-asp"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "sitecorepocfull"
   kind                = "Linux"
@@ -24,8 +24,8 @@ resource "azurerm_app_service_plan" "main" {
 
 # This creates the service definition
 resource "azurerm_app_service" "main" {
-  name                = "${var.prefix}-appservice"
-  location            = "${azurerm_resource_group.main.location}"
+  name                = var.prefix-appservice"
+  location            = azurerm_resource_group.main.location
   resource_group_name = "sitecorepocfull"
-  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+  app_service_plan_id = azurerm_app_service_plan.main.id
   }
