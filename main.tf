beba89f69bc93056685143e6c2c5831cad573e0a
SudhinNandan <57177849+SudhinNandan@users.noreply.github.com>
2019-11-29 10:02:42 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  version         = "=1.24.0"
+  subscription_id = "${var.subscription_id}"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "sitecorepocfull"
+  location = "${var.location}"
+}
+
+# This creates the plan that the service use
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.prefix}-asp"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "sitecorepocfull"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+# This creates the service definition
+resource "azurerm_app_service" "main" {
+  name                = "${var.prefix}-appservice"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "sitecorepocfull"
+  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+  }
