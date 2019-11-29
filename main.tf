edae5084902e1cf4746bae5ca89fcca218d65d4f
SudhinNandan <57177849+SudhinNandan@users.noreply.github.com>
2019-11-29 11:17:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "azurerm" {
-
+  version         = "=1.24.0"
   subscription_id = "da9510b8-3397-4012-a20c-328a1b93c19e"
 }
-
 
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
