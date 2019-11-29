1784a921f1fff43d97e79111761891669b360b44
SudhinNandan <57177849+SudhinNandan@users.noreply.github.com>
2019-11-29 10:51:56 +0000

    Update Variables.tf

--- Variables.tf
+++ Variables.tf
@@ -1,12 +1,7 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
-  default     = "SC"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID to be used for billing"
 }
